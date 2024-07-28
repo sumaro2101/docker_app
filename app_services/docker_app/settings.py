0cae96f98786eaed399a01edbc714521652be843
Alex Pavlov <yukiu217@gmail.com>
2024-07-28 16:52:02 +0600

    add readme

--- app_services/docker_app/settings.py
+++ app_services/docker_app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'HOST': os.environ.get('DB_HOST'),
+        'HOST': 'postgresql',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
